533aa604c9c872c7e9550aa9813b54760046b20c
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-09-05 18:22:22 -0300

    criando a estrutura do projeto

---
+++ src/test/java/com/desafioTecnicoJunior/Abastecimentos/AbastecimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioTecnicoJunior.Abastecimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbastecimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
