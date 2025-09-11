62d7a60d9f37fd875b1076c557442158ed6548d4
DouglasAVSantos <104694849+DouglasAVSantos@users.noreply.github.com>
2025-09-11 19:10:18 -0300

    Feat: criado a entidade de abastecimento

---
+++ src/main/java/com/desafioTecnicoJunior/Abastecimentos/model/Abastecimento.java
@@ -0,0 +1,48 @@
+package com.desafioTecnicoJunior.Abastecimentos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDateTime;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Abastecimento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "bomba_id")
+    private Bomba bomba;
+    private LocalDateTime data;
+    private BigDecimal valorTotal;
+    private BigDecimal litragem;
+
+    public Abastecimento(Bomba bomba, BigDecimal valorTotal, Boolean porValor) {
+
+        this.bomba = bomba;
+        this.data = LocalDateTime.now();
+        if (porValor) {
+            this.valorTotal = valorTotal;
+            this.litragem = valorTotal.divide(bomba.getTipo().getValorPorLitro(),3, RoundingMode.HALF_UP);
+        }
+        if (!porValor) {
+            this.valorTotal = valorTotal.multiply(bomba.getTipo().getValorPorLitro());
+            this.litragem = valorTotal;
+        }
+
+    }
+
+    public Abastecimento(Bomba bomba, BigDecimal valor) {
+        this(bomba, valor, true);
+    }
+
+}
